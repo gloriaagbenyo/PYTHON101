eeb09cbfc2e805e608dcf14cb4029014d93348ee
gloriaagbenyo <46813292+gloriaagbenyo@users.noreply.github.com>
2020-07-09 22:34:03 +0000

    Add files via upload

---
+++ Code101.py
@@ -0,0 +1,10 @@
+def Kindegarten():
+    name, age = "Princess", 6
+    address = "Adenta, Accra"
+    print("Name: {}\nAge: {}\nAddress: {}".format(name, age, address))
+Kindegarten()
+
+
+food = ["waakye", "rice", "kenkey"]
+for lunch in food:
+  print(lunch)
